e8a66ed1c174386ab5eb0173da1076787b48cd07
The Doctor <drwho@virtadpt.net>
2017-02-02 20:25:52 -0800

    Deleted the AUR_packages.txt file, because it's empty.
    Updated the set of explicitly installed packages.
    
    Added a UID 0 check for the set_up_kodi.sh script.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- mediabox/fakeroot/set_up_kodi.sh
+++ mediabox/fakeroot/set_up_kodi.sh
@@ -6,8 +6,19 @@
 #	PGP fingerprint: 7960 1CDC 85C9 0B63 8D9F  DD89 3BD8 FF2B 807B 17C1
 
 # Core code.
+# Checking for root.
+if [[ $EUID -ne 0 ]]; then
+    echo "You need to run this script as root.  Try again."
+    exit 1
+fi
+
+echo "Copying config files into place."
+cp X11/* /etc/X11
+cp lightdm/* /etc/lightdm
+cp pam.d/* /etc/pam.d
+
 echo "Adding Kodi service account."
-sudo useradd -c "Kodi Service Account" -G \
+useradd -c "Kodi Service Account" -G \
     dbus,network,video,audio,optical,storage,users -m kodiuser
 
 echo "Adding Kodi service groups."
